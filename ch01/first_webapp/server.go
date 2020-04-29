cbe929acbef99bad3f575425959238c003b6ff50
tishikawa <takayuki.ishikawa@gunosy.com>
2020-04-29 10:29:46 +0900

    Add comment to ch01/first_webapp/server.go

--- ch01/first_webapp/server.go
+++ ch01/first_webapp/server.go
@@ -1,16 +1,20 @@
 package main
 
-
 import (
 	"fmt"
 	"net/http"
 )
 
+// ハンドラ（handler）という言葉は、イベントがトリガー（きっかけ）として起動されるコールバック関数でよく使われる
 func handler(writer http.ResponseWriter, request *http.Request) {
-  fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+	// 接頭辞F が付いているものは、書き込み先を明示的に指定可能
+	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
 }
 
 func main() {
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
+	// 指定されたパターン（ここではルートURL（/））が呼び出された時、ハンドラが起動されるように設定する
+	// （内部的には ServeMux が起動している。ServeMux は HTTPリクエストマルチプレクサで、リクエストで飛んできたURLが登録済みのパターンにマッチした時に指定されたハンドラを呼び出す）
+	http.HandleFunc("/", handler)
+	// ポート8080を監視するようにサーバを起動する
+	http.ListenAndServe(":8080", nil)
 }
